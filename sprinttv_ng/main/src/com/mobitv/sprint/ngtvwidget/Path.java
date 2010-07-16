6b62792ee5b3ea5885e00826f4367a96331829a1
Ben Pellow <ben@mog.com>
2010-07-15 18:33:32 -0700

    Added comment to Path

--- sprinttv_ng/main/src/com/mobitv/sprint/ngtvwidget/Path.java
+++ sprinttv_ng/main/src/com/mobitv/sprint/ngtvwidget/Path.java
@@ -1,10 +1,12 @@
-package com.mobitv.sprint.ngtvwidget;	
+package com.mobitv.sprint.ngtvwidget;
+
+// test comment
 
 public class Path {
 	public String id;
-	public String name;				
-	
+	public String name;
+
 	public String toString() {
 		return "id: " + id + " name: " + name;
-	}		
+	}
 }
